58821eb5817516398b664c2f7c86d011a8b638c7
Crisktina <crishidalgolopez@gmail.com>
2022-12-04 22:27:26 +0100

    add products to cart

--- js/index.js
+++ js/index.js
@@ -1,35 +1,3 @@
-const productsCard = document.querySelector("#cards");
-
-function printProducts(productsArray) {
-  productsArray.forEach((item) => {
-    productsCard.innerHTML += `
-      <div class="item">
-          <div class="imgProduct">
-            <img src= "${item.image}" class="card-img-top" alt="${item.name}" />
-          </div>
-        <div class="infoProduct">
-          <div class="row1">
-            <h5>${item.name}</h5>
-            <a onclick="addToCart(${
-              item.id
-            })" href="#"><img src="assets/icons/buttonCart.svg" alt="Add product to cart" /></a>
-          </div>
-          <div class="row2">
-            <div class="quantity">
-              <button><img src="assets/icons/buttonMinus.svg" alt="More product" /></button>
-              <h6>01</h6>
-              <button><img src="assets/icons/buttonAdd.svg" alt="Less product" /></button>
-            </div>
-            <p>${item.price.toFixed(2)} €</p>
-          </div>
-        </div>
-      </div>
-  `;
-  });
-}
-
-printProducts(productsArray);
-
 // menu hamburger----------------------------
 const menuBtn = document.querySelector(".menuBtn");
 const nav = document.querySelector(".navMenu");
@@ -46,3 +14,73 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     nav.classList.remove("navMenuVisible");
   })
 );
+
+// RENDER PRODUCTS
+const productsCard = document.querySelector("#cards");
+
+function printProducts(productsArray) {
+  productsArray.forEach((item) => {
+    productsCard.innerHTML += `
+      <div class="item">
+          <div class="imgProduct">
+            <img src= "${item.image}" class="card-img-top" alt="${item.name}" />
+          </div>
+        <div class="infoProduct">
+          <div class="row1">
+            <h5>${item.name}</h5>
+            <a onclick="addToCart(${
+              item.id
+            })" ><img src="assets/icons/buttonCart.svg" alt="Add product to cart" /></a>
+          </div>
+          <div class="row2">
+            <div class="quantity">
+              <button onclick="addToCart(${
+                item.id
+              })"><img src="assets/icons/buttonMinus.svg" alt="More products" /></button>
+              <h6>${item.numberOfUnits}</h6>
+              <button onclick="addUnitBeforeSendToCart(${
+                item.id
+              })"><img src="assets/icons/buttonAdd.svg" alt="Less products" /></button>
+            </div>
+            <p>${item.price.toFixed(2)} €</p>
+          </div>
+        </div>
+      </div>
+  `;
+  });
+}
+
+printProducts(productsArray);
+
+// cart array
+let cart = [];
+
+// Number of units to put in cart
+// function addUnitBeforeSendToCart(id) {
+//   const item = productsArray.find((item) => item.id === id);
+//   productsArray.push({
+//     ...item,
+//     numberOfUnits: 1,
+//   });
+// }
+// addUnitBeforeSendToCart();
+
+// ADD TO CART
+function addToCart(id) {
+  // check if product already exist in cart
+  if (cart.some((product) => product.id === id)) {
+    Swal.fire({
+      title: "Product already in cart!",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  } else {
+    const product = productsArray.find((item) => item.id === id);
+    cart.push(product);
+    console.log(cart);
+  }
+}
